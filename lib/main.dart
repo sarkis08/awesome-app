bd93695a67e3f040a5799c977e56cb441b7b65ce
sarkis08 <moriehenryk@gmail.com>
2020-06-20 22:01:23 +0000

    Card, AssetImage,TextField & ScrollView

--- lib/main.dart
+++ lib/main.dart
@@ -12,14 +12,24 @@ void main() {
   );
 }
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({
     Key key,
   }) : super(key: key);
 
   @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+
+  var myText = "Change My Name";
+  TextEditingController _nameController = TextEditingController();
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.grey[100],
       appBar: AppBar(
         title: Text('Flutter App'),
       ),
@@ -32,10 +42,12 @@ class HomePage extends StatelessWidget {
             //   decoration: BoxDecoration(color: Colors.purple,),
             //   ),
             UserAccountsDrawerHeader(
-              accountName: Text('Henry Kormoh'), 
-              accountEmail: Text('exmaple@gmail.com'),
-              currentAccountPicture: CircleAvatar(backgroundImage: NetworkImage("https://webwiser.netlify.app/img/webwiser-sm.jpg"),)
-              ),
+                accountName: Text('Henry Kormoh'),
+                accountEmail: Text('exmaple@gmail.com'),
+                currentAccountPicture: CircleAvatar(
+                  backgroundImage: NetworkImage(
+                      "https://webwiser.netlify.app/img/webwiser-sm.jpg"),
+                )),
             ListTile(
               leading: Icon(Icons.person),
               title: Text('Henry Kormoh'),
@@ -53,15 +65,48 @@ class HomePage extends StatelessWidget {
         ),
       ),
       body: Center(
-        child: Container(
-          height: 100,
-          width: 100,
-          color: Colors.grey,
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: SingleChildScrollView(
+            child: Card(
+              color: Colors.purple[50],
+              child: Column(
+                children: <Widget>[
+                  Image.asset(
+                    "assets/james-harriso.jpg",
+                  ),
+                  SizedBox(
+                    height: 15,
+                  ),
+                  Text(
+                    myText,
+                    style: TextStyle(fontSize: 25),
+                  ),
+                  SizedBox(
+                    height: 15,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: TextField(
+                       controller: _nameController,
+                      decoration: InputDecoration(
+                          border: OutlineInputBorder(),
+                          hintText: "Enter Name",
+                          labelText: "Name"),
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ),
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        child: Icon(Icons.edit),
+        onPressed: () {
+          myText = _nameController.text;
+          setState(() {});
+        },
+        child: Icon(Icons.send),
         backgroundColor: Colors.purple,
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
